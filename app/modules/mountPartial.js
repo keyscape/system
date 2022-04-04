273aefcad4f4c0bd58c5a50c92e2ac948397eea2
Lucas Prado <pradoplucas@gmail.com>
2022-04-04 15:33:18 -0300

    upSession

--- app/modules/mountPartial.js
+++ app/modules/mountPartial.js
@@ -3,10 +3,15 @@ const fs = require('fs');
 const exphbs = require('express-handlebars'); /* Importa o módulo express-handlebars */
 
 var hbs = exphbs.create({});
-
+/* 
 hbs.handlebars.registerPartial('adminParticipants', hbs.handlebars.compile(fs.readFileSync(process.cwd() + '/app/views/partials/system10/adminParticipants.handlebars').toString('utf-8')));
 hbs.handlebars.registerPartial('adminModules', hbs.handlebars.compile(fs.readFileSync(process.cwd() + '/app/views/partials/system10/adminModules.handlebars').toString('utf-8')));
 hbs.handlebars.registerPartial('adminGroups', hbs.handlebars.compile(fs.readFileSync(process.cwd() + '/app/views/partials/system10/adminGroups.handlebars').toString('utf-8')));
+ */
+hbs.handlebars.registerPartial('adminParticipants', hbs.handlebars.compile(fs.readFileSync('/opt/system/app/views/partials/system10/adminParticipants.handlebars').toString('utf-8')));
+hbs.handlebars.registerPartial('adminModules', hbs.handlebars.compile(fs.readFileSync('/opt/system/app/views/partials/system10/adminModules.handlebars').toString('utf-8')));
+hbs.handlebars.registerPartial('adminGroups', hbs.handlebars.compile(fs.readFileSync('/opt/system/app/views/partials/system10/adminGroups.handlebars').toString('utf-8')));
+
 
 module.exports = {
     createAdminParticipants: (allParticipants) => {
